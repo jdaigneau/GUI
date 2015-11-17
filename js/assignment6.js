83d70e9029fee44652a73dcf3ee56473a24c51b3
Jeremy Daigneau <jdaigneau5@gmail.com>
2015-11-17 17:59:46 -0500

    Redid Assignment 7
    It works now

--- js/assignment6.js
+++ js/assignment6.js
@@ -4,87 +4,46 @@
  email          : jeremy_daigneau@student.uml.edu
  Code based from: https://github.com/Pkthunder/GUI-Help/blob/gh-pages/Assignment6/mult_table.js
  Assignment 6 Javascript for creating multiplication table.
+ Editted for Assignment 7
  */
 
-$(document).ready(function () {
-  
-  $("#form").submit(function () {
+  //function that calculates and generates the table
+  function createTable() {
 
-   $("#form").validate({
-    // Rules for validating the form.
-    rules: {
-      max_multiplicand: {
-        number: true,
-        min: -100,
-        max: 100,
-        required: true
-      },
-      min_multiplicand: {
-        number: true,
-        min: -100,
-        max: 100,
-        required: true
-      },
-      max_multiplier: {
-        number: true,
-        min: -100,
-        max: 100,
-        required: true
-      },
-      min_multiplier: {
-        number: true,
-        min: -100,
-        max: 100,
-        required: true
-      }
-    },
+    $("#result").empty(); //if table isn't empty, submit resets it
+    $("#swap-warning").empty(); //clears the div that would contain a swap warning
+    
+    //stores the input values as variables
+    var maxMultiplicand = $("#max_multiplicand").numVal();
+    var minMultiplicand = $("#min_multiplicand").numVal();
+    var maxMultiplier = $("#max_multiplier").numVal();
+    var minMultiplier = $("#min_multiplier").numVal();
 
-    // Error messages
-    messages: {
-      max_multiplicand: {
-        number: "Max Multiplicand Error: Please enter a number between -100 and 100. <br>",
-        min: "Max Multiplicand Error: Number is too small. Please enter a number greater than -100. <br>",
-        max: "Max Multiplicand Error: Number is too large. Please enter a number less than 100. <br>",
-        required: "Max Multiplicand Error: Please enter a number between -100 and 100.<br>"
-      },
-      min_multiplicand: {
-        number: "Min Multiplicand Error: Please enter a number between -100 and 100.<br>",
-        min: "Min Multiplicand Error: Number is too small. Please enter a number greater than -100. <br>",
-        max: "Min Multiplicand Error: Number is too large. Please enter a number less than 100.<br>",
-        required: "Min Multiplicand Error: Please enter a number between -100 and 100.<br>"
-      },
-      max_multiplier: {
-        number: "Max Multiplier Error: Please enter a number between -100 and 100.<br>",
-        min: "Max Multiplier Error: Number is too small. Please enter a number greater than -100.<br>",
-        max: "Max Multiplier Error: Number is too large. Please enter a number less than 100.<br>",
-        required: "Max Multiplier Error: Please enter a number between -100 and 100.<br>"
-      },
-      min_multiplier: {
-        number: "Min Multiplier Error: Please enter a number between -100 and 100.<br>",
-        min: "Min Multiplier Error: Number is too small. Please enter a number greater than -100.<br>",
-        max: "Min Multiplier Error: Number is too large. Please enter a number less than 100.<br>",
-        required: "Min Multiplier Error: Please enter a number between -100 and 100.<br>"
-      }
-    },
-    
-   errorPlacement : function( error, element ) {
-      $(error).insertAfter( $("#form") ) ;
-     },
-    
-     invalidHandler: function() {
-      // Wipe the previous table / error messages
-      // This way nothing will show up if a user tries to submit with an error.
-      $(".error").empty();
-      $("#result").empty();
-    }
-    
-  });
-
-    $("#result").empty(); //if table is empty, submit resets it
+    //if the max is less than the min, swap them
+     if (maxMultiplicand < minMultiplicand) {
+       
+       //prints messages saying values were swapped
+       $("#swap-warning").append("<p> Swapped multiplicand values. </p>");
+       
+    	var tmp = maxMultiplicand;
+        maxMultiplicand = minMultiplicand;
+        minMultiplicand = tmp;
+     }
+     
+     //stores the input values as variables
+     if (maxMultiplier < minMultiplier) {
+       
+       //prints messages saying values were swapped
+       $("#swap-warning").append("<p> Swapped multiplier values. </p>");
+       
+        var tmp = maxMultiplier;
+        maxMultiplier = minMultiplier;
+        minMultiplier = tmp;
+     }
 
     //gets column and row values from forms
-    var columns = ($("#max_multiplicand").numVal() - $("#min_multiplicand").numVal()) + 2;
-    var rows = ($("#max_multiplier").numVal() - $("#min_multiplier").numVal()) + 2;
+    var columns = (maxMultiplicand - minMultiplicand) + 2;
+    var rows = (maxMultiplier - minMultiplier) + 2;
 
     var appendStr = ""; //assign appendStr as a string which will store the table
 
@@ -96,28 +55,29 @@ $(document).ready(function () {
       for (var j = 0; j < Math.abs(columns); j++) { //loops through columns
 
         if (i === 0 && j === 0) {
+          
           //top left corner, put place holder
           appendStr = appendStr.concat("<th> * </th>");
-        }
-
-        else if (i === 0) {
+          
+        } else if (i === 0) {
+          
           //get value of j for multiplicand on side
           appendStr = appendStr.concat("<th>");
-          appendStr = appendStr.concat((j - 1 + $("#min_multiplicand").numVal()).toString());
+          appendStr = appendStr.concat((j - 1 + minMultiplicand).toString());
           appendStr = appendStr.concat("</th>");
-        }
-
-        else if (j === 0) {
+          
+        } else if (j === 0) {
+          
           //get value for i for multiplier on top
           appendStr = appendStr.concat("<th>");
-          appendStr = appendStr.concat((i - 1 + $("#min_multiplier").numVal()).toString());
+          appendStr = appendStr.concat((i - 1 + minMultiplier).toString());
           appendStr = appendStr.concat("</th>");
-        }
-
-        else {
+          
+        } else {
+          
           //populate the table
           appendStr = appendStr.concat("<td>");
-          appendStr = appendStr.concat(((i - 1 + $("#min_multiplier").numVal()) * (j - 1 + $("#min_multiplicand").numVal())).toString());
+          appendStr = appendStr.concat(((i - 1 + minMultiplier) * (j - 1 + minMultiplicand)).toString());
           appendStr = appendStr.concat("</td>");
         }
 
@@ -126,9 +86,91 @@ $(document).ready(function () {
     }
 
     $("#result").append(appendStr); //append the DOM
-    $(".error").empty();
+   
+  };
+
+function validate() {
+
+  //specifies the form to validate
+  $("#form").validate({
+   
+    //form validation rules
+    rules: {
+      min_multiplicand: {
+        number: true,
+        min: -10,
+        max: 10,
+        required: true
+      },
+      max_multiplicand: {
+        number: true,
+        min: -10,
+        max: 10,
+        required: true
+      },
+      min_multiplier: {
+        number: true,
+        min: -10,
+        max: 10,
+        required: true
+      },
+      max_multiplier: {
+        number: true,
+        min: -10,
+        max: 10,
+        required: true
+      }
+    },
+    
+    // Error messages for invalid form inputs
+    messages: {
+      min_multiplicand: {
+        number: "Min Multiplicand ERROR: Please enter a number between 1 and 10.",
+        min: "Min Multiplicand ERROR: Number too small. Please enter a number between 1 and 10. ",
+        max: "Min Multiplicand ERROR: Number too large. Please enter a number between 1 and 10.",
+        required: "Min Multiplicand ERROR: Please enter a number between 1 and 10."
+      },
+      max_multiplicand: {
+        number: "Max Multiplicand ERROR: Please enter a number between 1 and 10.",
+        min: "Max Multiplicand ERROR: Number too small. Please enter a number between 1 and 10. ",
+        max: "Max Multiplicand ERROR: Number too large. Please enter a number between 1 and 10.",
+        required: "Max Multiplicand ERROR: Please enter a number between 1 and 10."
+      },
+      max_multiplier: {
+        number: "Max Multiplier ERROR: Please enter a number between 1 and 10.",
+        min: " Max Multiplier ERROR: Number too small. Please enter a number between 1 and 10. ",
+        max: "Max Multiplier ERROR: Number too large. Please enter a number between 1 and 10.",
+        required: "Max Multiplier ERROR: Please enter a number between 1 and 10."
+      },
+      min_multiplier: {
+        number: "Min Multiplier ERROR: Please enter a number between 1 and 10.",
+        min: "Min Multiplier ERROR: Number too small. Please enter a number between 1 and 10. ",
+        max: "Min Multiplier ERROR: Number too large. Please enter a number between 1 and 10.",
+        required: "Min Multiplier ERROR: Please enter a number between 1 and 10."
+      }
+    },
+    
+    // Valid form function
+    submitHandler: function () {
+      
+      createTable(); //creates table
+      return false; 
+    },
+    
+    //inva;id form function
+    invalidHandler: function () {
+      
+     $("#swap-warning").empty(); //clears warning
+     $("#result").empty(); //clears table
+    },
+    
+   //places the error messages after the input
+    errorElement: "div",
+    errorPlacement: function (error, element) {
+      error.appendTo( element.parent("div"));
+    }
   });
-});
+}
 
 //return an input tag's value as a number(or int)
 jQuery.fn.numVal = function () {
